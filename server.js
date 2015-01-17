ce8d3faa884b7cf742e83202f9dfdbe186cdc2a7
Daniel Cohen <danielc@ironsrc.com>
2015-01-17 11:39:52 +0200

    moved project to node express & ejs, restructred to fit with new libs, home page design is complete, fix repeate isssue with home page download section

---
+++ server.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Daniel on 1/17/2015.
+ */
+// load the things we need
+var express = require('express'),
+    compression = require('compression'),
+    app = express();
+
+// Express uses
+app.use(compression()); // Compression (Gzip)
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index/home page
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+app.listen(80);
+console.log('Express.js server is listening on http://localhost:80');
