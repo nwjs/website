069894f33b488a49e7dd1dfa9e217fffd1e014a1
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2015-12-23 17:39:06 -0500

    Cleaned up main folder
    Removed bower as all it was doing was importing Normalize, which can
    better be handled with Sass. Updated config file to 12.3.

--- config.js
+++ config.js
@@ -3,34 +3,34 @@
  */
 module.exports = {
     github_link: 'https://github.com/nwjs/nw.js',
-    version_title: 'v0.12.0-alpha1',
+    version_title: 'v0.12.3',
     download32Bit: [
         {
             label: 'Windows 32bit',
-            link: 'http://dl.nwjs.io/v0.12.0-alpha1/nwjs-v0.12.0-alpha1-win-ia32.zip'
+            link: 'http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-win-ia32.zip'
         },
         {
             label: 'Mac 32bit',
-            link: 'http://dl.nwjs.io/v0.12.0-alpha1/nwjs-v0.12.0-alpha1-osx-ia32.zip'
+            link: 'http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-osx-ia32.zip'
         },
         {
             label: 'Linux 32bit',
-            link: 'http://dl.nwjs.io/v0.12.0-alpha1/nwjs-v0.12.0-alpha1-linux-ia32.tar.gz'
+            link: 'http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-linux-ia32.tar.gz'
         }
     ],
     download64Bit: [
         {
             label: 'Windows 64bit',
-            link: 'http://dl.nwjs.io/v0.12.0-alpha1/nwjs-v0.12.0-alpha1-win-x64.zip'
+            link: 'http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-win-x64.zip'
         },
         {
             label: 'Mac 64bit',
-            link: 'http://dl.nwjs.io/v0.12.0-alpha1/nwjs-v0.12.0-alpha1-osx-x64.zip'
+            link: 'http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-osx-x64.zip'
         },
         {
             label: 'Linux 64bit',
-            link: 'http://dl.nwjs.io/v0.12.0-alpha1/nwjs-v0.12.0-alpha1-linux-x64.tar.gz'
+            link: 'http://dl.nwjs.io/v0.12.3/nwjs-v0.12.3-linux-x64.tar.gz'
         }
     ],
-    release_notes: 'https://groups.google.com/d/msg/nwjs-general/qWJczc-E92E/9lmIiMfysU8J'
+    release_notes: 'https://groups.google.com/d/msg/nwjs-general/hhXCS4aXGV0/TUQmcu5XDwAJ'
 };
