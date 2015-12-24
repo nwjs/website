292f133a9b70644cb502ae0da8c410b429b76303
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2015-12-24 09:14:41 -0500

    Relative path

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
 
-    $.get("../_markup/header.html", function (headerData) {
+    $.get("_markup/header.html", function (headerData) {
         $("header").html(headerData);
     });
 
-    $.get("../_markup/footer.html", function (footerData) {
+    $.get("_markup/footer.html", function (footerData) {
         $("footer").html(footerData);
     });
 
