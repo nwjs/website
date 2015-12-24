867921d3d6b21ad7c247f6c7147b2b807800c278
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2015-12-24 00:12:34 -0500

    Began converting it to a functional gh-pages site
    moving stuff around, renaming things. Getting rid of EJS in exchange for
    html and regular javascript and jquery

--- server.js
+++ server.js
@@ -1,49 +1,82 @@
-/**
- * Created by Daniel on 1/17/2015.
- */
-// load the things we need
-var express = require('express'),
-    compression = require('compression'),
-    config = require('./config'),
-    fs = require('fs'),
-    marked = require('marked'),
-    app = express();
+//This is needed in order to test 404.html, router.js, and api.html
 
-// Express uses
-app.use(compression()); // Compression (Gzip)
-app.use(express.static(__dirname + '/public'));
+//NPM-Free Server by The Jared Wilcurt
+//All you need to run this is an installed copy of Node.JS
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+//Require in some of the native stuff that comes with Node
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+//Port number to use
+var port = process.argv[2] || 8000;
+//Colors for CLI output
+var WHT = "\033[39m";
+var RED = "\033[91m";
+var GRN = "\033[32m";
 
-// Function for rendering Markdown file
-var markdown = function (filename) {
-    var markdownFile = fs.readFileSync(__dirname + '/views/docs/' + filename, 'utf8');
-    markdownFile = marked(markdownFile);
+//Create the server
+http.createServer(function (request, response) {
 
-    return markdownFile;
-};
+    //The requested URL like http://localhost:8000/file.html
+    var uri = url.parse(request.url).pathname;
+    //get the file.html from above and then find it from the current folder
+    var filename = path.join(process.cwd(), uri);
 
-// index/home page
-app.get('/', function (req, res) {
-    res.render('pages/index', config);
-});
+    //Setting up MIME-Types
+    var contentTypesByExtension = {
+        '.html': "text/html",
+        '.css':  "text/css",
+        '.js':   "text/javascript"
+    };
 
-app.get('/download', function(req, res) {
-    res.render('pages/download', config);
-});
+    //Check if the requested file exists
+    fs.exists(filename, function(exists) {
+        //If it doesn't
+        if (!exists) {
+            //Output a red error pointing to failed request
+            console.log(RED + "FAIL: " + filename);
+            //Redirect the browser to the 404 page
+            filename = path.join(process.cwd(), '/404.html');
+        //If the requested URL is a folder, like http://localhost:8000/catpics
+        } else if (fs.statSync(filename).isDirectory()) {
+            //Output a green line to the console explaining what folder was requested
+            console.log(GRN + "FLDR: " + WHT + filename);
+            //redirect the user to the index.html in the requested folder
+            filename += '/index.html';
+        }
 
-app.get('/blog', function(req, res) {
-    res.render('pages/blog', config);
-});
+        //Assuming the file exists, read it
+        fs.readFile(filename, "binary", function(err, file) {
+            //Output a green line to console explaining the file that will be loaded in the browser
+            console.log(GRN + "FILE: " + WHT + filename);
+            //If there was an error trying to read the file
+            if (err) {
+                //Put the error in the browser
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
 
-app.get('/quickstart', function(req, res) {
-    res.render('pages/quickstart', {markdown: markdown});
-});
+            //Otherwise, declar a headers object and a var for the MIME-Type
+            var headers = {};
+            var contentType = contentTypesByExtension[path.extname(filename)];
+            //If the requested file has a matching MIME-Type
+            if (contentType) {
+                //Set it in the headers
+                headers["Content-Type"] = contentType;
+            }
 
-app.get('/docs', function(req, res) {
-    res.render('pages/docs', config);
-});
+            //Output the read file to the browser for it to load
+            response.writeHead(200, headers);
+            response.write(file, "binary");
+            response.end();
+        });
 
-app.listen(8000);
-console.log('Express.js server is listening on http://localhost:8000');
+    });
+
+}).listen(parseInt(port, 10));
+
+//Message to display when server is started
+console.log(WHT + "Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
