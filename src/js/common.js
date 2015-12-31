d1cebf9bb77940d4299608ae94bb83ddb0f39756
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2015-12-31 16:26:05 -0500

    Correct line endings, make spacing consistent
    Many files did not have a return at the end to allow for proper
    cross-platform version control. Some files had 2 spaces, some had 4,
    some had tabs, and some had combinations of all three. I've converted
    all files to 4 spaces for a more consistent codebase.

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,17 @@
 function getJSON(url, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.send();
-  xhr.onload = function() {
-    try {
-      cb(null, JSON.parse(xhr.responseText));
-    } catch (e) {
-      cb(e);
-    }
-  };
-  xhr.onerror = cb;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.onload = function() {
+        try {
+            cb(null, JSON.parse(xhr.responseText));
+        } catch (e) {
+            cb(e);
+        }
+    };
+    xhr.onerror = cb;
 }
 
 function getVersionsJSON(cb) {
-  getJSON('/versions.json', cb);
+    getJSON('/versions.json', cb);
 }
