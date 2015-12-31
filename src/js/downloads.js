cec25ed556ca495a1222b161a7b28b818331a070
Cong Liu <cong.liu@intel.com>
2015-12-31 12:13:50 +0800

    Implemented Downloads page

--- src/js/downloads.js
+++ src/js/downloads.js
@@ -1,94 +1,104 @@
 (function() {
 
-  var OSNav = React.createClass({
-    render: function() {
-
+  function ext(file) {
+    switch(file) {
+    case 'win-x64':
+    case 'win-ia32':
+      return '.zip';
+    case 'osx-x64':
+      return '.zip';
+    case 'linux-x64':
+    case 'linux-ia32':
+      return '.tar.gz';
     }
-  });
+  }
 
-  var ArchNav = React.createClass({
-    render: function() {
+  var file2name = {
+    'win-ia32'  : 'Windows 32-bit',
+    'win-x64'   : 'Windows 64-bit',
+    'osx-x64'   : 'Mac OS X 64-bit',
+    'linux-x64' : 'Linux 64-bit',
+    'linux-ia32': 'Linux 32-bit'
+  };
 
-    }
-  });
+  function name(file) {
+    return file2name[file];
+  }
 
-  var FlavorsNav = React.createClass({
-    render: function() {
-
-    }
-  });
-
-  function ext(file, symbol) {
-    if (symbol) {
-      switch(file) {
-      case 'win-x64':
-      case 'win-ia32':
-        return '.7z';
-      case 'mac-x64':
-        return '.zip';
-      case 'linux-x64':
-      case 'linux-ia32':
-        return '.tar.gz';
+  function findVersion(versions, v) {
+    var verList = versions.versions;
+    var idx = -1;
+    verList.some(function(item, i) {
+      if (item.version === v) {
+        idx = i;
+        return true;
       }
+    });
+    if (idx >= 0) {
+      return verList[idx];
     } else {
-      switch(file) {
-      case 'win-x64':
-      case 'win-ia32':
-        return '.zip';
-      case 'mac-x64':
-        return '.zip';
-      case 'linux-x64':
-      case 'linux-ia32':
-        return '.tar.gz';
-      }
+      return false;
     }
   }
 
-  function name(type) {
-    switch(type) {
-    case 'chromedriver':
-      return 'ChromeDriver';
-    case 'symbol':
-      return 'Symbol';
-    case 'header':
-      return 'Header';
-    case 'lib':
-      return 'Lib';
-    default:
-      return 'NW.js';
-    }
-  }
-
-  var flavors = [];
-
-  var DownloadItem = React.createClass({
+  var DownloadMatrix = React.createClass({
     render: function() {
       var baseUrl = this.props.base;
       var version = this.props.version;
-      var flavors = this.props.flavors;
-      var cols = flavors.map(function(flavor) {
-        var url = baseUrl + '/nwjs-' + (flavor === 'normal' ? '': '-' + flavor)
-         + '-' + version.version
-         + '-' + file
-         + ext(file);
-        return <td><a href={url}>{name()}</a></td>
+      var flavors = version.flavors;
+      var widthStyle = {width:(100 / (flavors.length + 1)) + '%'};
+      var rows = version.files.map(function(file, rI) {
+        var cols = flavors.filter(function(flavor) {
+          if (flavor === 'macappstore' || flavor === 'mas') {
+            if (!file.match(/osx/i)) {
+              return false;
+            }
+          }
+          return true;
+        })
+        .map(function(flavor, cI) {
+          var url = baseUrl + '/' + version.version + '/nwjs' + (flavor === 'normal' ? '': ('-' + flavor))
+           + '-' + version.version
+           + '-' + file
+           + ext(file);
+          return (<td key={cI} style={widthStyle}><a href={url}>{flavor.toUpperCase()}</a></td>);
+        });
+        cols.unshift(<th key={-1} style={widthStyle}>{name(file)}</th>);
+
+        return (<tr key={rI}>{cols}</tr>);
       });
-      return <tr><th>{version.version}</th>{cols}</tr>;
+      return (<table><tbody>{rows}</tbody></table>);
     }
   });
 
   var Downloads = React.createClass({
+    getInitialState: function() {
+      return {version: 'stable'};
+    },
     render: function() {
       var versions = this.props.versions;
       var baseUrl = this.props.base;
       return <div>
-      <div class="nav">
-        <OSNav versions={versions}/>
-        <ArchNav versions={versions}/>
-        <FlavorsNav versions={versions}/>
-      </div>
-      <DownloadItem base={baseUrl} versions={versions}/>
+        <nav>
+          <a href="#" className={this.state.version === 'stable'? 'selected': ''} onClick={this.toggleVersion}><h3>Stable</h3><br/>{versions.stable}</a>
+          <a href="#" className={this.state.version === 'latest'? 'selected': ''} onClick={this.toggleVersion}><h3>Latest</h3><br/>{versions.latest}</a>
+        </nav>
+        <DownloadMatrix base={baseUrl} version={findVersion(versions, versions[this.state.version])}/>
+        <ul>
+          <li><a href={baseUrl + '/' + versions[this.state.version] + '/'}>Other downloads</a> for {versions[this.state.version]}</li>
+          <li><a href={baseUrl}>Previous releases</a></li>
+          <li><a href={baseUrl + '/live-build/'}>Nightly builds</a></li>
+        </ul>
       </div>;
+    },
+    toggleVersion: function(e) {
+      var version = this.state.version;
+      if (version === 'stable') {
+        this.setState({version: 'latest'});
+      } else {
+        this.setState({version: 'stable'});
+      }
+      e.preventDefault();
     }
   });
 
